830aa39531f6ea808e5069d83c7a06d45c39dc92
Shivansh Raghav <shivanshraghav7@gmail.com>
2023-08-29 03:21:17 +0000

    sdsd

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,21 @@
 
-import React from "react";
+import React,{useEffect,useState} from "react";
 import './../styles/App.css';
+import axios from "axios";
+import 'regenerator-runtime/runtime'
 
 const App = () => {
+  let [data,Setdata]=useState('')
+  useEffect(()=>{
+    (async ()=>{
+      let res=await axios.get('https://dummyjson.com/products')
+      console.log(res.data)
+      Setdata(JSON.stringify(res.data.products))
+    })()
+  },[])
   return (
     <div>
-        {/* Do not remove the main div */}
+        {data? <pre>{data}</pre> : ''}
     </div>
   )
 }
